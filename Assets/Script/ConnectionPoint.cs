82208bfb2293140113d347ca95dd3b2808f6976a
Gyb <479276132@qq.com>
2022-08-04 22:20:03 +0800

    修复连接线bug
    因为ConnectionPoint类错误地继承了MonoBehaviour

--- Assets/Script/ConnectionPoint.cs
+++ Assets/Script/ConnectionPoint.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum ConnectionPointType { In,Out}
-public class ConnectionPoint : MonoBehaviour
+public class ConnectionPoint
 {
     public Rect rect;
     public ConnectionPointType type;
